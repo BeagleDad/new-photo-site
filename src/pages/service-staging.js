4ddc94096820d70e8409e333a07f4f3177873cff
Matt Hoffman <formatt.hoffman@gmail.com>
2023-11-26 17:25:10 -0700

    initial commit

---
+++ src/pages/service-staging.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from "../components/layout";
+import { Container } from 'react-bootstrap';
+const serviceVirtualStaging = () => {
+  return (
+    <Layout>
+      <Container>
+        <h1>Virtual Staging Services</h1>
+        
+        <p>
+          Cupidatat pariatur minim dolor cupidatat Lorem magna. Cillum ullamco aute nisi cillum mollit excepteur cillum consequat adipisicing dolor. Elit in minim eiusmod culpa proident minim minim aliquip voluptate sit tempor adipisicing dolore. Pariatur amet quis proident excepteur ea magna id.
+        </p>
+        
+      </Container>
+    </Layout>
+  )
+}
+
+export default serviceVirtualStaging
