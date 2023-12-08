d14bf5cb42fc0623a5e74c7d0e7a118601528206
Matt Hoffman <formatt.hoffman@gmail.com>
2023-12-07 17:49:54 -0700

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import Layout from "../components/layout";
+import React from "react"
+import Layout from "../components/layout"
 import Seo from "../components/seo"
-
 
 const contact = () => {
   return (
     <Layout>
-      <h1>Contact</h1>
-      
+      <h2 className='text-center'>Contact</h2>
       <p>
-        Cupidatat pariatur minim dolor cupidatat Lorem magna. Cillum ullamco aute nisi cillum mollit excepteur cillum consequat adipisicing dolor. Elit in minim eiusmod culpa proident minim minim aliquip voluptate sit tempor adipisicing dolore. Pariatur amet quis proident excepteur ea magna id.
+        Thank you for your interest. Please contact me to setup a photo shoot or
+        with any questions.
       </p>
+      <div>
+      <i className="bi bi-envelope pe-3"></i>matthoffmanphoto@gmail.com
+      </div>
+      <div>
+      <i className="bi bi-telephone  pe-3"></i>928-550-8604
+      </div>
+
     </Layout>
   )
 }
