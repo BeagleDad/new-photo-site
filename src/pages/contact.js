797546f69261346ec26b61790e834770f924cbb1
Matt Hoffman <formatt.hoffman@gmail.com>
2023-12-11 14:02:16 -0700

    Added email form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,57 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const contact = () => {
   return (
     <Layout>
-      <h2 className='text-center'>Contact</h2>
+      <h2 className="text-center">Contact</h2>
       <p>
         Thank you for your interest. Please contact me to setup a photo shoot or
-        with any questions.
+        with any questions. You can contact me directly via email, phone, text, or fill
+        out the form below to send me an email.
       </p>
       <div>
-      <i className="bi bi-envelope pe-3"></i>matthoffmanphoto@gmail.com
+        <i className="bi bi-envelope pe-3"></i>matthoffmanphoto@gmail.com
       </div>
       <div>
-      <i className="bi bi-telephone  pe-3"></i>928-550-8604
+        <i className="bi bi-telephone  pe-3"></i>928-550-8604
       </div>
-
+     
+     <h5 className="mt-5">Email Form</h5>
+      <Form>
+        <Row>
+        <Col>
+          <Form.Control placeholder="First name" />
+        </Col>
+        <Col>
+          <Form.Control placeholder="Last name" />
+        </Col>
+      </Row>
+        {/* <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Name" />
+        </Form.Group> */}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPhone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control type="phone" placeholder="Phone Number" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Enter Message</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </Layout>
   )
 }
