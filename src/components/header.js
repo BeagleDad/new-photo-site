658a9fb117058d599e1b108f49b0c99965e6888c
Matt Hoffman <formatt.hoffman@gmail.com>
2023-12-13 14:14:42 -0700

    Cleanup

--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,72 @@
 import * as React from "react"
-import { Link } from "gatsby";
-import  { StaticImage }  from "gatsby-plugin-image";
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import ThemeSwitcher from "./ThemeSwitcher";
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import ThemeSwitcher from "./ThemeSwitcher"
 
 const Header = ({ siteTitle }) => (
   <Navbar expand="lg" /* fixed="top" */ className="bg-body-tertiary">
-      <Container>
+    <Container>
       <Navbar.Brand as={Link} to="/" className="">
-          <StaticImage src="../images/mh_logo_round.svg" width={40} alt="logo" className="me-3 "/>
-          <Navbar.Text className="fs-3">Matt Hoffman Photography</Navbar.Text>
+        <StaticImage
+          src="../images/mh_logo_round.svg"
+          width={40}
+          alt="logo"
+          className="me-3 "
+        />
+        <Navbar.Text className="fs-3">Matt Hoffman Photography</Navbar.Text>
       </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <NavDropdown title="Portfolio" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/portfolio-interior">Interior</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/portfolio-exterior">Exterior</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/portfolio-aerial">Aerial</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Services" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/service-property">Property Photos</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/service-aerial">Aerial Photos</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/service-staging">Virtual Staging</NavDropdown.Item>
-              {/* <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item> */}
-            </NavDropdown>
-            <NavDropdown title="Info">
-              <NavDropdown.Item as={Link} to="/info-prep"><i class="bi bi-house-check pe-2"></i>Property Prep</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/info-terms"><i class="bi bi-pen pe-2"></i>Terms of Service</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          </Nav>
-          <ThemeSwitcher />
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <NavDropdown title="Portfolio" id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} to="/portfolio-interior">
+              Interior
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/portfolio-exterior">
+              Exterior
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/portfolio-aerial">
+              Aerial
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Services" id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} to="/service-property">
+              Property Photos
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/service-aerial">
+              Aerial Photos
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/service-staging">
+              Virtual Staging
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Info">
+            <NavDropdown.Item as={Link} to="/info-prep">
+              <i class="bi bi-house-check pe-2"></i>Property Prep
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/info-terms">
+              <i class="bi bi-pen pe-2"></i>Terms of Service
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <ThemeSwitcher />
+    </Container>
+  </Navbar>
 )
 
 export default Header
