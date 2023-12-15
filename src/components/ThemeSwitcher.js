7781049459b16cdbe8badbfdb0835a28fa23749e
Matt Hoffman <formatt.hoffman@gmail.com>
2023-12-15 14:53:03 -0700

    fixed incorrect use of class instead of className.

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -39,7 +39,7 @@ const ThemeSwitcher = () => {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-toggle-on"
+          className="bi bi-toggle-on"
           viewBox="0 0 16 16"
         >
           <path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8" />
