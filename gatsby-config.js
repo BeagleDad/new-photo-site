56a5b1aebed73b923f6be384fa86cfd5607f0b8b
Matt Hoffman <formatt.hoffman@gmail.com>
2023-12-15 15:58:09 -0700

    Changed description, etc and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Matt Hoffman Photography`,
+    description: `Beautiful, professional photographs for marketing real estate. Serving Prescott, Prescott Valley, and surrounding area.`,
+    author: `@matthoffman`,
+    siteUrl: `https://matthoffmanphoto.com/`,
   },
   pathPrefix: "/new-photo-site",
   plugins: [
@@ -29,15 +29,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Matt Hoffman Photography`,
+        short_name: `MHP`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mh_favicon.png`, // This path is relative to the root of the site.
       },
     },
   ],
