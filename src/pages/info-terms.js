0ba358dec2ba719b6400e55d4469a9fbc0e7edb3
Matt Hoffman <formatt.hoffman@gmail.com>
2023-12-17 14:49:37 -0700

    Added logo to terms of service pdf.

--- src/pages/info-terms.js
+++ src/pages/info-terms.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import terms_pdf from "../assets/terms.pdf"
+import terms_pdf from "../assets/terms_of_service.pdf"
 const terms = () => {
   return (
     <Layout>
