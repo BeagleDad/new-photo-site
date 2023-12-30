05e0259cfe453fef7da0fb45b0aa4a38039576b5
Matt Hoffman <formatt.hoffman@gmail.com>
2023-12-30 11:46:57 -0700

    Changed wording for page not found.

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,13 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&#39;t exist...</p>
+    <Link to="/">Return to Home Page</Link>
   </Layout>
 )
 
